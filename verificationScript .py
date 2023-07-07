52c3d71ce33957b30269f36a4c568514ef81477e
sahidAle <sahidalejandroo@gmail.com>
2023-07-06 19:17:33 -0600

    Clase para crear archivos de excel (relaciones de clientes y avales)

--- verificationScript .py
+++ verificationScript .py
@@ -1,5 +1,6 @@
 import csv
 from abc import ABC, abstractmethod
+from openpyxl import Workbook
 
 class CSVReader(ABC):
     @abstractmethod
@@ -8,29 +9,31 @@ class CSVReader(ABC):
 
 class ExcelCSVReader(CSVReader):
     def read_csv_file(self, filename_csv):
-        csv_data = csv.reader(open(filename_csv))
-        next(csv_data)
-        next(csv_data)
-        next(csv_data)
+        with open(filename_csv) as file:
+            csv_data = csv.reader(file)
+            next(csv_data)
+            next(csv_data)
+            next(csv_data)
 
-        client_names = set()
-        aval_names = set()
-        for row in csv_data:
-            full_name_client = row[2] + " " + row[3] + " " + row[4]
-            full_name_aval = row[408] + " " + row[409] + " " + row[410]
-            client_names.add(full_name_client)
-            aval_names.add(full_name_aval)
+            client_names = set()
+            aval_names = set()
+            for row in csv_data:
+                full_name_client = row[2] + " " + row[3] + " " + row[4]
+                full_name_aval = row[408] + " " + row[409] + " " + row[410]
+                client_names.add(full_name_client)
+                aval_names.add(full_name_aval)
 
         return client_names, aval_names
 
 class OdooCSVReader(CSVReader):
     def read_csv_file(self, filename_csv):
-        csv_data = csv.reader(open(filename_csv))
-        next(csv_data)
+        with open(filename_csv) as file:
+            csv_data = csv.reader(file)
+            next(csv_data)
 
-        rows = set()
-        for row in csv_data:
-            rows.add(row[0])
+            rows = set()
+            for row in csv_data:
+                rows.add(row[0])
 
         return rows
 
@@ -43,50 +46,74 @@ class ListComparer:
 
         return result
 
-class ResultPrinter:
-    def print_match(self, result):
+class ExcelCreator:
+    def __init__(self, workbook):
+        self.workbook = workbook
+
+    def create_excel(self, file_name, data, source_of_comparison):
+        try:
+            sheet = self.workbook.active
+
+            total_records, match, no_match = self.get_matches(data)
+            print("Result", file_name, total_records, match, no_match)
+
+            sheet['A1'] = 'Nombre'
+            sheet['B1'] = '¿Se encuentra en %s?' % source_of_comparison
+            sheet['D1'] = "Total de personas"
+            sheet['E1'] = "Coincidencias"
+            sheet['F1'] = "No Coincidencias"
+            sheet['D2'] = total_records
+            sheet['E2'] = match
+            sheet['F2'] = no_match
+
+            for i, dato in enumerate(data, start=2):
+                sheet[f'A{i}'] = dato[0]
+                sheet[f'B{i}'] = dato[1]
+
+            full_file_name = file_name + ".xlsx"
+            self.workbook.save(full_file_name)
+            print("Archivo %s creado con éxito\n" % full_file_name)
+        except Exception as e:
+            print("Error:", str(e))
+
+    def get_matches(self, data):
         match = 0
         no_match = 0
-        for item in result:
+        for item in data:
             if item[1]:
                 match += 1
             else:
                 no_match += 1
 
-        print("Matches:", match)
-        print("No matches:", no_match)
-
-excel_csv_reader = ExcelCSVReader()
-odoo_csv_reader = OdooCSVReader()
-
-csvExcelClient, csvExcelAval = excel_csv_reader.read_csv_file("BD DINERO XPRESS 2023. CRUDA SEMANA 25 CSV.csv")
-
-csvOdooClient = odoo_csv_reader.read_csv_file("Préstamo (dev.loan.loan).csv")
-csvOdooAval = odoo_csv_reader.read_csv_file("Contacto (res.partner).csv")
-
-len(csvExcelAval)
+        return len(data), match, no_match
 
 
-print("Total de clientes en el csv crudo:", len(csvExcelClient))
-print("Total de clientes en el csv de Odoo:", len(csvOdooClient))
-print("Total de avales en el csv crudo:", len(csvExcelAval))
-print("Total de avales en el csv de Odoo", len(csvOdooAval))
+def main():
+    excel_csv_reader = ExcelCSVReader()
+    odoo_csv_reader = OdooCSVReader()
 
-resultExcelClient = ListComparer().compare_lists(csvExcelClient, csvOdooClient)
-resultOdooClient = ListComparer().compare_lists(csvOdooClient, csvExcelClient)
-resultExcelAval = ListComparer().compare_lists(csvExcelAval, csvOdooAval)
-resultOdooAval = ListComparer().compare_lists(csvOdooAval, csvExcelAval)
+    csv_semana_cruda_client, csv_semana_cruda_aval = excel_csv_reader.read_csv_file("BD DINERO XPRESS 2023. CRUDA SEMANA 25 CSV.csv")
 
-result_printer = ResultPrinter()
+    csv_odoo_client = odoo_csv_reader.read_csv_file("Préstamo (dev.loan.loan).csv")
+    csv_odoo_aval = odoo_csv_reader.read_csv_file("Contacto (res.partner).csv")
 
-print("\nClientes en el Excel:")
-result_printer.print_match(resultExcelClient)
-print("\nClientes en Odoo:")
-result_printer.print_match(resultOdooClient)
-print("\nAvales en el Excel:")
-result_printer.print_match(resultExcelAval)
-print("\nAvales en el Odoo: ")
-result_printer.print_match(resultOdooAval)
+    excel_clients_with_odoo = ListComparer().compare_lists(csv_semana_cruda_client, csv_odoo_client)
+    odoo_clients_with_excel = ListComparer().compare_lists(csv_odoo_client, csv_semana_cruda_client)
+
+    excel_avals_with_odoo = ListComparer().compare_lists(csv_semana_cruda_aval, csv_odoo_aval)
+    odoo_avals_with_excel = ListComparer().compare_lists(csv_odoo_aval, csv_semana_cruda_aval)
+
+    workbook = Workbook()
+    excel_creator = ExcelCreator(workbook)
+
+    excel_creator.create_excel("excel_clients_in_odoo", excel_clients_with_odoo, "Odoo")
+    excel_creator.create_excel("excel_avals_in_odoo", excel_avals_with_odoo, "Odoo")
+
+    excel_creator.create_excel("odoo_clients_in_excel", odoo_clients_with_excel, "Excel")
+    excel_creator.create_excel("odoo_avals_in_excel", odoo_avals_with_excel, "Excel")
+
+    workbook.close()
 
 
-#print(resultOdooAval)
+if __name__ == "__main__":
+    main()
